c70fb8876ddd472022cc5d5f4e4e2ee6b0b12e57
Adisorn1999 <blackcopter007@gmail.com>
2022-12-07 00:17:04 +0700

    Update app.js
    optimize API Login and Register

--- app.js
+++ app.js
@@ -1,148 +1,176 @@
-var express = require('express')
-var cors = require('cors')
-var bodyParser = require('body-parser')
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-var jwt = require('jsonwebtoken');
-const secret = 'dsme';
-const HttpStatus = require('http-status-codes');
+var jwt = require("jsonwebtoken");
+const secret = "dsme";
+const HttpStatus = require("http-status-codes");
+const {
+  json
+} = require("body-parser");
 
-var app = express()
+var app = express();
 var jsonParser = bodyParser.json();
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
 //connection database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'dsme'
+  host: "localhost",
+  user: "root",
+  database: "dsme",
+
 });
 
-app.get('/', jsonParser, function (req, res, next) {
+app.get("/", jsonParser, function (req, res, next) {
   res.json({
-    msg: 'This is origins!'
-  })
-})
+    msg: "This is origins!",
+  });
+});
 
 // Api Register
-app.post('/register', jsonParser, function (req, res, next) {
-  let username = req.body.username;
-  let password = req.body.password;
-  let first_name = req.body.first_name;
-  let last_name = req.body.last_name;
-  let birthday = req.body.birthday;
+app.post("/register", jsonParser, function (req, res, next) {
+  try { // Ger user input
+    const {
+      username,
+      password,
+      first_name,
+      last_name,
+      birthday
+    } = req.body;
 
-  if (!username || !password || !first_name || !last_name || !birthday) {
-    res.json({
-      ok: false,
-      message: 'Please complete the information.',
-      code: HttpStatus.StatusCodes.BAD_REQUEST
-    });
+    //validate user input
+    if (!(username && password && first_name && last_name && birthday)) {
+      res.send({
+        ok: false,
+        message: "Please complete the information.",
+        code: HttpStatus.StatusCodes.BAD_REQUEST,
+      });
+    } else {
+      bcrypt.hash(password, saltRounds, function (err, hash) {
+        connection.execute(
+          "INSERT INTO users(username, password, first_name, last_name, birthday) VALUES (?,?,?,?,?)",
+          [username, hash, first_name, last_name, birthday],
+          function (err, users, fields) {
+            // check if user already exist
+            // validate if user exist in our database
+            if (err) {
+              return res.send({
+                ok: false,
+                message: "Username already taken.",
+                code: HttpStatus.StatusCodes.BAD_REQUEST,
+                Text: err.message,
+              });
+            }
+            res.send({
+              ok: true,
+              message: "Register success.",
+              code: HttpStatus.StatusCodes.OK,
+            });
+          }
+        );
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    connection.execute('INSERT INTO users(username, password, first_name, last_name, birthday) VALUES (?,?,?,?,?)',
-      [username, hash, first_name, last_name, birthday],
-      function (err, users, fields) {
-        if (err){
-          return res.json({
-            ok: false,
-            message: 'Username already taken.',
-            code: HttpStatus.StatusCodes.BAD_REQUEST
-          });
-        } 
-        res.json({
-          ok: true,
-          message: 'Register success.',
-          code: HttpStatus.StatusCodes.OK
-        })
-      })
-  });
-
-
 });
 
 //Api Login
-app.post('/login', jsonParser, function (req, res, next) {
+app.post("/login", jsonParser, function (req, res, next) {
   try {
-    let username = req.body.username;
-    connection.execute(
-      'SELECT * FROM `users` WHERE  username = ?',
-      [username],
-      function (err, users, fields) {
-        if (err) {
-          res.json({
-            ok: false,
-            message: err,
-            code: HttpStatus.StatusCodes.BAD_REQUEST
-          })
-        }
-        if (users.length == 0) {
-          res.json({
-            ok: false,
-            message: 'Invalid username or password!.',
-            token,
-            code: HttpStatus.StatusCodes.UNAUTHORIZED
-          })
-        }
-        bcrypt.compare(req.body.password, users[0].password, function (err, result) {
-          // result == true
-          if (result) {
-            var token = jwt.sign({
-              username: users[0].username
-            }, secret, {
-              expiresIn: '1y'
-            });
-            res.json({
-              ok: true,
-              message: 'login success.',
-              token,
-              code: HttpStatus.StatusCodes.OK
-            })
-          } else {
+    const {
+      username,
+      password
+    } = req.body;
+    if (!(username && password)) {
+      res.json({
+        ok: false,
+        message: "Please complete the information.",
+        code: HttpStatus.StatusCodes.BAD_REQUEST,
+      });
+    } else {
+      connection.execute(
+        "SELECT * FROM `users` WHERE  username = ?",
+        [username],
+        function (err, users, fields) {
+          if (err) {
             res.json({
               ok: false,
-              message: 'Invalid username or password!.',
-              code: HttpStatus.StatusCodes.UNAUTHORIZED
-            })
+              message: err,
+              code: HttpStatus.StatusCodes.BAD_REQUEST,
+            });
           }
-        });
-      }
-    );
+          if (users.length == 0) {
+            return res.json({
+              ok: false,
+              message: "Invalid username or password!.",
+              code: HttpStatus.StatusCodes.UNAUTHORIZED,
+            });
+          }
+          bcrypt.compare(password, users[0].password, function (err, result) {
+            // result == true
+            if (err) throw err;
+            if (result) {
+              var token = jwt.sign({
+                  username: users[0].username,
+                },
+                secret, {
+                  expiresIn: "1h",
+                }
+              );
+              res.json({
+                ok: true,
+                message: "login success.",
+                token,
+                code: HttpStatus.StatusCodes.OK,
+              });
+            } else {
+              res.json({
+                ok: false,
+                message: "Invalid username or password!.",
+                code: HttpStatus.StatusCodes.UNAUTHORIZED,
+              });
+            }
+          });
+        }
+      );
+    }
   } catch (error) {
     console.log(error);
     res.send({
       ok: false,
       error: error.message,
-      code: HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR
+      code: HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR,
     });
-
   }
-})
+});
 
 // Api authen check token
-app.post('/authen', jsonParser, function (req, res, next) {
+app.post("/authen", jsonParser, function (req, res, next) {
   try {
-    const token = req.headers.authorization.split(' ')[1]
+    const token = req.headers.authorization.split(" ")[1];
     var decoded = jwt.verify(token, secret);
     res.json({
       ok: true,
-      message: 'success.',
-      token,
-      code: HttpStatus.StatusCodes.OK
-    })
+      message: "success.",
+      decoded,
+      code: HttpStatus.StatusCodes.OK,
+    });
   } catch (err) {
     res.json({
       ok: false,
       message: err.message,
-      code: HttpStatus.StatusCodes.BAD_REQUEST
-    })
+      code: HttpStatus.StatusCodes.BAD_REQUEST,
+    });
   }
-
-})
+});
 
 app.listen(3000, function () {
-  console.log('web server listening on port 3000')
-})
+  console.log("web server listening on port 3000");
+});
