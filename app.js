76288d4344b42949ee3058c8a9555b59095dbfbf
Adisorn1999 <blackcopter007@gmail.com>
2022-12-20 17:53:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 var jwt = require("jsonwebtoken");
-const secret = "dsme";
+const secret = "1EEA6DC-JAM4DP2-PHVYPBN-V0XCJ9X";
 const HttpStatus = require("http-status-codes");
-const {
-  json
-} = require("body-parser");
+const { json } = require("body-parser");
 
 var app = express();
 var jsonParser = bodyParser.json();
@@ -24,7 +22,6 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "dsme",
-
 });
 
 app.get("/", jsonParser, function (req, res, next) {
@@ -35,14 +32,9 @@ app.get("/", jsonParser, function (req, res, next) {
 
 // Api Register
 app.post("/register", jsonParser, function (req, res, next) {
-  try { // Ger user input
-    const {
-      username,
-      password,
-      first_name,
-      last_name,
-      birthday
-    } = req.body;
+  try {
+    // Ger user input
+    const { username, password, first_name, last_name, birthday } = req.body;
 
     //validate user input
     if (!(username && password && first_name && last_name && birthday)) {
@@ -84,10 +76,7 @@ app.post("/register", jsonParser, function (req, res, next) {
 //Api Login
 app.post("/login", jsonParser, function (req, res, next) {
   try {
-    const {
-      username,
-      password
-    } = req.body;
+    const { username, password } = req.body;
     if (!(username && password)) {
       res.json({
         ok: false,
@@ -117,10 +106,12 @@ app.post("/login", jsonParser, function (req, res, next) {
             // result == true
             if (err) throw err;
             if (result) {
-              var token = jwt.sign({
+              var token = jwt.sign(
+                {
                   username: users[0].username,
                 },
-                secret, {
+                secret,
+                {
                   expiresIn: "1h",
                 }
               );
@@ -168,6 +159,204 @@ app.post("/authen", jsonParser, function (req, res, next) {
       message: err.message,
       code: HttpStatus.StatusCodes.BAD_REQUEST,
     });
+  }
+});
+
+// get users
+app.get("/users", jsonParser, function (req, res, next) {
+  connection.execute(
+    "SELECT `user_id`, `username`, `first_name`, `last_name`, `birthday` FROM `users`",
+    (err, result, fields) => {
+      if (err) throw err;
+      res.json({
+        ok: true,
+        data: result,
+      });
+    }
+  );
+});
+
+//get users by  user_id
+app.get("/users/:id", jsonParser, function (req, res, next) {
+  try {
+    var id = req.params.id;
+    connection.execute(
+      "SELECT `user_id`, `username`, `first_name`, `last_name`, `birthday` FROM `users` WHERE user_id = ?",
+      [id],
+      (err, result, fields) => {
+        if (err) throw err;
+        if (id) {
+          res.json({
+            ok: true,
+            data: result,
+            code: HttpStatus.StatusCodes.OK,
+          });
+        }
+      }
+    );
+  } catch (err) {
+    // thow message error (command)
+    console.log(err);
+    res.json({
+      ok: false,
+      message: err.message,
+      code: HttpStatus.StatusCodes.SERVICE_UNAVAILABLE,
+    });
+  }
+});
+
+app.get("/username/:name", jsonParser, function (req, res, next) {
+  var name = req.params.name;
+  connection.execute(
+    "SELECT `user_id`, `username`, `first_name`, `last_name`, `birthday` FROM `users` WHERE username = ?",
+    [name],
+    (err, result, fields) => {
+      if (err) throw err;
+      if (result.length == 0) {
+        res.json({
+          ok: false,
+          message: "User not found ",
+          code: HttpStatus.StatusCodes.SERVICE_UNAVAILABLE,
+        });
+      } else {
+        res.json({
+          ok: true,
+          result,
+          code: HttpStatus.StatusCodes.OK,
+        });
+      }
+    }
+  );
+});
+// get blood sugar all
+app.get("/bloods", jsonParser, function (req, res) {
+  try {
+    connection.execute("SELECT * FROM `blood`", (err, result) => {
+      if (err) throw err;
+      return res.json({
+        ok: true,
+        data: result,
+        code: HttpStatus.StatusCodes.OK,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+//get blood sugar by user_id
+app.get("/blood/:id", jsonParser, (req, res) => {
+  try {
+    const id = req.params.id;
+    connection.execute(
+      "SELECT * FROM `blood` WHERE user_id = ?",
+      [id],
+      (err, result) => {
+        if (err) throw err;
+        return res.json({
+          ok: true,
+          data: result,
+          code: HttpStatus.StatusCodes.OK,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+// add blood sugar
+app.post("/blood/:id", jsonParser, function (req, res) {
+  try {
+    const { blood_level, blood_time } = req.body;
+    const user_id = req.params.id;
+    if (!(blood_level && blood_time && user_id)) {
+      res.json({
+        ok: false,
+        message: "1Please complete the information.",
+      });
+    } else {
+      connection.execute(
+        "INSERT INTO `blood`( `blood_level`, `blood_time`, `user_id`) VALUES (?,?,?) ",
+        [blood_level, blood_time, user_id],
+        (err, users, fields) => {
+          if (err) throw err;
+          return res.send({
+            ok: true,
+            message: " success.",
+            code: HttpStatus.StatusCodes.OK,
+          });
+        }
+      );
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+//get medication all
+app.get("/medications", jsonParser, (req, res) => {
+  try {
+    connection.execute(
+      "SELECT * FROM `medication_warehouse`",
+      (err, result) => {
+        if (err) throw err;
+        return res.json({
+          ok: true,
+          data: result,
+          code: HttpStatus.StatusCodes.OK,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+// get medication by user_id
+app.get("/medication/:id", jsonParser, (req, res) => {
+  try {
+    const id = req.params.id;
+    connection.execute(
+      "SELECT * FROM `medication_warehouse` WHERE user_id = ?",
+      [id],
+      (err, result, fields) => {
+        if (err) throw err;
+        return res.json({
+          ok: true,
+          data: result,
+          code: HttpStatus.StatusCodes.OK,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+// add medication
+app.post("/medication/:id", jsonParser, function (req, res) {
+  try {
+    const { medication_name, medication_amount, medication_time, time } =
+      req.body;
+    const user_id = req.params.id;
+    if (!(medication_name, medication_amount, medication_time, time, user_id)) {
+      res.json({
+        ok: false,
+        message: "1Please complete the information.",
+        code: HttpStatus.StatusCodes.BAD_REQUEST,
+      });
+    } else {
+      connection.execute(
+        "INSERT INTO `medication_warehouse`( `medication_name`, `medication_amount`, `medication_time`, `time`, `user_id`) VALUES (?,?,?,?,?) ",
+        [medication_name, medication_amount, medication_time, time, user_id],
+        (err, users, fields) => {
+          if (err) throw err;
+          return res.send({
+            ok: true,
+            message: " success.",
+            code: HttpStatus.StatusCodes.OK,
+          });
+        }
+      );
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
 
